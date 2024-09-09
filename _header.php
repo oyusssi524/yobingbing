4eef897f2a67ae00d7c7eeb3b7bbe36c9bc28e1a
oyusssi524 <oyusssi524@gmail.com>
2024-09-09 12:35:55 +0900

    Create files for ybb settings

---
+++ _header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>요거트 아이스크림 전문점 : 요빙빙</title>
+  <!-- Favicons -->
+	<link rel="shortcut icon" href="./assets/img/favicon.ico">
+  <!-- common.css -->
+  <link rel="stylesheet" href="./assets/css/common.css">
+  <!-- bootstrap css v5.3.2 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- bootstrap icon v1.11.3 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <!-- google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>  
+  <header class="bz-header"></header>
