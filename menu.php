2d1c528cf4e4d286d4b1f0336a871717d736746c
oyusssi524 <oyusssi524@gmail.com>
2024-09-10 15:50:52 +0900

    Update menu.php change class name

--- menu.php
+++ menu.php
@@ -5,12 +5,12 @@ include_once "./_header.php"
     <!-- 요거트 아이스크림 메뉴 소개 -->
     <section class="bz-sec">
       <div class="container">
-        <div class="row">
-          <div class="bz-tit-box bz-tit-left col-12">
+        <div class="row row-cols-1">
+          <div class="bz-tit-box bz-tit-left col">
             <h2 class="bz-tit">요거트 아이스크림</h2>
             <p class="bz-tit-caption">요빙빙은 '건강한 즐거움'이라는 브랜드 철학을 바탕으로, 고품질 원료와 혁신적인 레시피를 통해 맛과 건강을 동시에 추구합니다.</p>
           </div>
-          <div class="bz-item-box col-12">
+          <div class="col bz-item-box">
             <div class="row row-cols-1 row-cols-lg-2">
               <div class="col">
                 <div class="bz-tit-box">
@@ -36,12 +36,12 @@ include_once "./_header.php"
     <!-- 시그니처 메뉴 소개 -->
     <section class="bz-sec">
       <div class="container">
-        <div class="row">
-          <div class="bz-tit-box bz-tit-right col-12">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box bz-tit-right">
             <h2 class="bz-tit">시그니처 메뉴</h2>
             <p class="bz-tit-caption">간편하게 주문할 수 있는 인기 메뉴 제작</p>
           </div>
-          <div class="bz-item-box col-12">
+          <div class="col bz-item-box">
             <div class="row row-cols-2 row-cols-lg-5">
               <div class="col">
                 <div class="bz-tit-box">
@@ -107,12 +107,12 @@ include_once "./_header.php"
     <!-- 토핑 소개-->
     <section class="bz-sec">
       <div class="container">
-        <div class="row">
-          <div class="bz-tit-box bz-tit-left col-12">
+        <div class="row row-cols-1">
+          <div class="bz-tit-box bz-tit-left col">
             <h2 class="bz-tit">골라먹는 토핑</h2>
             <p class="bz-tit-caption">간다양한 토핑을 곁들여 나만의 메뉴 완성</p>
           </div>
-          <div class="bz-item-box col-12">
+          <div class="col bz-item-box">
             <div class="row row-cols-4 row-cols-lg-6">
               <div class="col">
                 <div class="bz-tit-box">
