b99b7bbd2b1e904e911e00e86a68922d7df02c0d
oyusssi524 <oyusssi524@gmail.com>
2024-09-10 16:06:38 +0900

    Create about.php and write contents

---
+++ about.php
@@ -0,0 +1,67 @@
+<?php
+include_once "./_header.php"
+?>
+  <main class="bz-main">
+    <!-- Brand Story -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box bz-tit-right">
+            <h2 class="bz-tit">Brand Story</h2>
+          </div>
+          <div class="col bz-item-box">
+            <div class="row row-cols-1 row-cols-lg-2 flex-lg-row-reverse">
+              <div class="col">
+                <div class="bz-img-box">
+                  <img src="./assets/img/no_img_rect_w.png" alt="브랜드 소개" class="img-fluid">
+                </div>
+              </div>
+              <div class="col">
+                <div class="bz-tit-box bz-tit-left">
+                  <h3 class="bz-tit">프리미엄 요거트 아이스크림, <span>요빙빙</span></h3>
+                </div>
+                <div class="bz-txt-box">
+                  <p class="bz-txt">신선한 요거트와 풍부한 토핑으로 완성된 나만의 특별한 디저트! 상큼한 맛과 건강함을 동시에 즐길 수 있는 요거트 아이스크림 전문점에서, 매일 신선한 재료로 만든 부드러운 요거트 아이스크림을 만나보세요. 다양한 토핑과 시그니처 소스로 나만의 개성을 담은 디저트를 완성해보세요. 건강과 맛, 모두를 만족시키는 최고의 디저트 경험을 선사합니다!</p>
+                  <p class="bz-txt">이동 중에도 변함없는 맛! 시간을 고려한 레시피 설계로 언제나 신선하고 균형 잡힌 맛을 즐기실 수 있습니다.</p>
+                  <p class="bz-txt">시간과 맛, 모두를 만족시키는 섬세한 요거트 아이스크림의 차이를 느껴보세요!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- BI -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box bz-tit-left">
+            <h2 class="bz-tit">BI</h2>
+          </div>
+          <div class="col bz-item-box">
+            <div class="row row-cols-1 row-cols-lg-2">
+              <div class="col">
+                <div class="bz-tit-box">
+                  <h3 class="bz-tit">로고 디자인</h3>
+                </div>
+                <div class="bz-img-box">
+                  <img src="./assets/img/no_img_rect_w.png" alt="로고 디자인" class="img-fluid">
+                </div>
+              </div>
+              <div class="col">
+                <div class="bz-tit-box">
+                  <h3 class="bz-tit">브랜드 컬러</h3>
+                </div>
+                <div class="bz-img-box">
+                  <img src="./assets/img/no_img_rect_w.png" alt="브랜드 컬러" class="img-fluid">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+<?php
+include_once "./_footer.php"
+?>
