7e14442559f150e845787aadc7de94b41daff011
oyusssi524 <oyusssi524@gmail.com>
2024-09-10 10:54:39 +0900

    Update _footer.php and css

--- _footer.php
+++ _footer.php
@@ -1,4 +1,19 @@
-  <footer class="bz-footer"></footer>
+<footer class="bz-footer bg-dark">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="bz-ft-logo col-lg-3">
+          <img src="./assets/img/ybb-fc-logo.png" alt="요빙빙-가맹사업본부-로고">
+        </div>
+        <div class="bz-ft-info col-lg-9">
+          <div class="bz-ft-info-item d-lg-flex justify-content-lg-end">
+            <p class="bz-ft-address"><span>주소&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;</span>서울시 광진구 면목로 41</p>
+            <p class="bz-ft-call"><span>문의&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;</span>1800-5279</p>
+          </div>
+          <p class="bz-ft-copyright">Copyrightⓒ요빙빙가맹사업본부.All Right Reserved.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 <script src="/assets/js/common.js"></script>
