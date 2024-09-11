0c46350f46c679a92e0f126e9360cfcf75a1a92f
oyusssi524 <oyusssi524@gmail.com>
2024-09-11 14:04:23 +0900

    Create franchise.php and write contents

---
+++ franchise.php
@@ -0,0 +1,296 @@
+<?php
+include_once "./_header.php"
+?>
+  <main class="bz-main">
+    <!-- 요빙빙 소개 -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box bz-tit-lg-right">
+            <h2 class="bz-tit">요빙빙 소개</h2>
+          </div>
+          <div class="col bz-item-box">
+            <div class="row row-cols-1 row-cols-lg-2 flex-lg-row-reverse">
+              <div class="col">
+                <div class="bz-img-box">
+                  <img src="./assets/img/no_img_rect_w.png" alt="요빙빙 소개" class="img-fluid">
+                </div>
+              </div>
+              <div class="col">
+                <div class="bz-tit-box bz-tit-lg-left">
+                  <h3 class="bz-tit">요빙빙 : 프리미엄 요거트 아이스크림</h3>
+                </div>
+                <div class="bz-txt-box">
+                  <p class="bz-txt">신선한 맛과 건강한 즐거움이 만나는 곳, 요빙빙에서 프리미엄 요거트 아이스크림의 세계로 여러분을 초대합니다. 혁신적인 맛과 트렌디한 분위기로 고객의 마음을 사로잡는 요빙빙과 함께 성공적인 프랜차이즈의 꿈을 이뤄보세요.</p>
+                </div>
+                <div class="bz-btn-box">
+                  <a href="" class="bz-btn">창업 상담 신청하기</a>
+                  <a href="" class="bz-btn">브로셔 다운로드</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- 요거트 아이스크림 시장 분석 -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box bz-tit-lg-left">
+            <h2 class="bz-tit">요거트 아이스크림 시장</h2>
+          </div>
+          <div class="col bz-item-box">
+            <div class="row row-cols-1 row-cols-lg-2">
+              <div class="col">
+                <div class="img-box">
+                  <img src="./assets/img/no_img_rect_h.png" alt="요거트 아이스크림 시장" class="img-fluid">
+                </div>
+              </div>
+              <ul class="col">
+                <li class="bz-li-box">
+                  <i class="bi bi-1-square-fill bi-num-square"></i>
+                  <h4 class="bz-tit">시장 성장</h4>
+                  <p class="bz-txt">요거트 아이스크림 시장은 최근 5년간 연평균 15% 성장률을 보이며 급속도로 확대되고 있습니다. 건강에 대한 관심 증가와 디저트 문화의 확산으로 앞으로도 지속적인 성장이 예상됩니다.</p>
+                </li>
+                <li class="bz-li-box">
+                  <i class="bi bi-2-square-fill bi-num-square"></i>
+                  <h4 class="bz-tit">소비자 트렌드</h4>
+                  <p class="bz-txt">20-30대 젊은 층을 중심으로 SNS 공유가 가능한 인스타그래머블한 디저트에 대한 수요가 증가하고 있습니다. 또한 프로바이오틱스 등 건강 기능성을 강조한 제품에 대한 관심도 높아지고 있습니다.</p>
+                </li>
+                <li class="bz-li-box">
+                  <i class="bi bi-3-square-fill bi-num-square"></i>
+                  <h4 class="bz-tit">경쟁 환경</h4>
+                  <p class="bz-txt">대형 프랜차이즈와 로컬 브랜드 간의 경쟁이 치열해지고 있으며, 차별화된 맛과 브랜드 아이덴티티가 성공의 핵심 요소로 부각되고 있습니다. 요빙빙은 이러한 시장 환경에 최적화된 전략으로 성공적인 진입을 준비하고 있습니다.</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- 요빙빙이 특별한 이유 -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box">
+            <h2 class="bz-tit">요빙빙이 특별한 이유</h2>
+          </div>
+          <ul class="col bz-item-box">
+            <li class="bz-txt">“남녀노소 부담없이 즐길 수 있는 상큼한 맛”</li>
+            <li class="bz-txt">“임대료 부담없는 주걱지 중심의 골목상권”</li>
+            <li class="bz-txt">“이동 시간을 고려해 완벽히 조율된 레시피”</li>
+            <li class="bz-txt">“체계화된 창업 시스템으로 빠르고 저렴하게”</li>
+            <li class="bz-txt">“매장 운영관리 시스템으로 수익 극대화”</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <!-- 수익 구조 -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box">
+            <h2 class="bz-tit">안정적인 수익 구조</h2>
+          </div>
+          <div class="col bz-item-box">
+            <div class="bz-img-box">
+              <img src="./assets/img/no_img_rect_w.png" alt="수익구조" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- 마케팅 전략 -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box">
+            <h2 class="bz-tit">요빙빙만의 마케팅 전략</h2>
+          </div>
+          <div class="col bz-item-box">
+            <ul id="accordionItemBox" class="accordion">
+              <li class="accordion-item">
+                <h3 class="accordion-header">
+                  <a class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">소셜 미디어 마케팅</a>
+                </h3>
+                <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionItemBox">
+                  <p class="accordion-body">인스타그램, 페이스북, 틱톡 등 다양한 SNS 채널을 활용한 마케팅을 전개합니다. 인플루언서 협업, 해시태그 캠페인, 사용자 제작 콘텐츠(UGC) 이벤트 등을 통해 브랜드 인지도를 높이고 고객과의 소통을 강화합니다.</p>
+                </div>
+              </li>
+              <li class="accordion-item">
+                <h3 class="accordion-header">
+                  <a class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">로컬 마케팅</a>
+                </h3>
+                <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionItemBox">
+                  <p class="accordion-body">각 지역 특성에 맞는 맞춤형 프로모션과 이벤트를 진행합니다. 지역 축제 참여, 학교 및 기업과의 제휴 마케팅, 커뮤니티 참여 활동 등을 통해 지역사회와의 유대관계를 강화하고 브랜드 충성도를 높입니다.</p>
+                </div>
+              </li>
+              <li class="accordion-item">
+                <h3 class="accordion-header">
+                  <a class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">디지털 마케팅</a>
+                </h3>
+                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionItemBox">
+                  <p class="accordion-body">SEO/SEM을 통한 검색 최적화, 리타겟팅 광고, 이메일 마케팅 등 다양한 디지털 마케팅 기법을 활용합니다. 빅데이터 분석을 통해 타겟 고객층의 행동 패턴을 파악하고 맞춤형 마케팅 전략을 수립합니다.</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- 인테리어 -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box">
+            <h2 class="bz-tit">인테리어</h2>
+          </div>
+          <div class="col bz-item-box">
+            <div class="row">
+              <div class="col-lg-8"><img src="./assets/img/no_img_rect_w.png" alt="인테리어"></div>
+              <div class="col-lg-4">
+                <div class="bz-txt-box">
+                  <p class="bz-txt">깔끔하고 모던한 미니멀리즘 디자인을 적용하여 제품의 신선함과 청결함을 강조합니다. 화이트 톤의 벽면과 가구에 민트색 포인트를 더해 브랜드 아이덴티티를 표현합니다.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- 창업 절차 -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box">
+            <h2 class="bz-tit">창업 절차</h2>
+          </div>
+          <div class="col bz-item-box">
+            <div class="d-flex flex-column flex-lg-row justify-content-between">
+              <div class="d-flex-item">
+                <i class="bi bi-chat-square-text"></i>
+                <p class="bz-txt">가맹상담</p>
+              </div>
+              <div class="d-flex-item">
+                <i class="bi bi-geo-alt-fill"></i>
+                <p class="bz-txt">점포입지분석</p>
+              </div>
+              <div class="d-flex-item">
+                <i class="bi bi-clipboard-check"></i>
+                <p class="bz-txt">가맹 계약</p>
+              </div>
+              <div class="d-flex-item">
+                <i class="bi bi-hammer"></i>
+                <p class="bz-txt">시설공사</p>
+              </div>
+              <div class="d-flex-item">
+                <i class="bi bi-book"></i>
+                <p class="bz-txt">본사 교육</p>
+              </div>
+              <div class="d-flex-item">
+                <i class="bi bi-ui-checks"></i>
+                <p class="bz-txt">오픈 리허설</p>
+              </div>
+              <div class="d-flex-item">
+                <i class="">OPEN</i>
+                <p class="bz-txt">OPEN</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- 창업 비용 -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box">
+            <h2 class="bz-tit">창업 비용</h2>
+          </div>
+          <div class="col bz-item-box">
+            <div class="bz-tab-content">
+              <ul id="pyeong_tab" class="nav nav-tabs bz-tab-head" role="tablist">
+                <li class="nav-item">
+                  <a id="pyeong10_tab" class="nav-link active" data-bs-toggle="tab" data-bs-target="#pyeong10" type="button" role="tab" aria-controls="pyeong10" aria-selected="true">10평</a>
+                </li>
+                <li class="nav-item">
+                  <a id="pyeong15_tab" class="nav-link" data-bs-toggle="tab" data-bs-target="#pyeong15" type="button" role="tab" aria-controls="pyeong15" aria-selected="false">15평</a>
+                </li>
+              </ul>
+              <ul id="pyeong" class="tab-content bz-tab-body">
+                <li id="pyeong10" class="tab-pane fade show active" role="tabpanel" aria-labelledby="pyeong10_tab" tabindex="0">
+                  <img src="./assets/img/no_img_sq.png" alt="10평 개설비용" class="img-fluid">
+                </li>
+                <li id="pyeong15" class="tab-pane fade" role="tabpanel" aria-labelledby="pyeong15_tab" tabindex="0">
+                  <img src="./assets/img/no_img_rect_w.png" alt="10평 개설비용" class="img-fluid">
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- 추가 혜택 -->
+    <section class="bz-sec">
+      <div class="container">
+        <div class="row row-cols-1">
+          <div class="col bz-tit-box">
+            <h2 class="bz-tit">추가 혜택</h2>
+          </div>
+          <div class="col bz-item-box">
+            <div class="row row-cols-2 row-cols-lg-5">
+              <div class="col">
+                <div class="bz-img-box">
+                  <img src="./assets/img/no_img_sq.png" alt="배달 앱 세팅" class="img-fluid">
+                </div>
+                <div class="bz-txt-box">
+                  <h5 class="bz-tit">배달 앱 세팅</h5>
+                  <p class="bz-txt">무료</p>
+                </div>
+              </div>
+              <div class="col">
+                <div class="bz-img-box">
+                  <img src="./assets/img/no_img_sq.png" alt="네이버 플레이스 등록" class="img-fluid">
+                </div>
+                <div class="bz-txt-box">
+                  <h5 class="bz-tit">네이버 플레이스 등록</h5>
+                  <p class="bz-txt">무료</p>
+                </div>
+              </div>
+              <div class="col">
+                <div class="bz-img-box">
+                  <img src="./assets/img/no_img_sq.png" alt="상권분석" class="img-fluid">
+                </div>
+                <div class="bz-txt-box">
+                  <h5 class="bz-tit">상권분석</h5>
+                  <p class="bz-txt">무료</p>
+                </div>
+              </div>
+              <div class="col">
+                <div class="bz-img-box">
+                  <img src="./assets/img/no_img_sq.png" alt="온라인 홍보채널 제작" class="img-fluid">
+                </div>
+                <div class="bz-txt-box">
+                  <h5 class="bz-tit">온라인 홍보채널 제작</h5>
+                  <p class="bz-txt">무료</p>
+                </div>
+              </div>
+              <div class="col">
+                <div class="bz-img-box">
+                  <img src="./assets/img/no_img_sq.png" alt="전부지원자금 컨설팅" class="img-fluid">
+                </div>
+                <div class="bz-txt-box">
+                  <h5 class="bz-tit">전부지원자금 컨설팅</h5>
+                  <p class="bz-txt">무료</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+<?php
+include_once "./_footer.php"
+?>
